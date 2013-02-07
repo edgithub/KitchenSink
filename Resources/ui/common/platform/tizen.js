f3c5f14b626ab88f7203c0397c8ea362c683667d
Roman Kamenetsky <roman.kamenetskyi@globallogic.com>
2013-02-07 17:37:51 +0200

    Added KitchenSink->Platform->Tizen with updates according to code standards

--- Resources/ui/common/platform/tizen.js
+++ Resources/ui/common/platform/tizen.js
@@ -4,21 +4,21 @@ function tizen(_args) {
 			{ title: 'Contacts', hasChild: true, test: 'ui/handheld/tizen/platform/contacts' },
 			{ title: 'Call', hasChild: true, test: 'ui/handheld/tizen/platform/call/call' },
 			{ title: 'Geocoder', hasChild: true, test: 'ui/handheld/tizen/platform/geocoder' },		
-			{ title: 'Application', hasChild: true, test: 'ui/handheld/tizen/platform/applications/tizen_application' },
-			{ title: 'Filesystem', hasChild: true, test: 'ui/handheld/tizen/platform/tizen_filesystem' },
-			{ title: 'Download demo', hasChild: true, test: 'ui/handheld/tizen/platform/tizenDownload' },
-			{ title: 'System info and Power', hasChild: true, test: 'ui/handheld/tizen/platform/tizenSystemInfo' },
-			{ title: 'NFC demo', hasChild: true, test: 'ui/handheld/tizen/platform/tizenNFC' },
+			{ title: 'Application', hasChild: true, test: 'ui/handheld/tizen/platform/application/application' },
+			{ title: 'Filesystem', hasChild: true, test: 'ui/handheld/tizen/platform/filesystem' },
+			{ title: 'Download demo', hasChild: true, test: 'ui/handheld/tizen/platform/tizen_download' },
+			{ title: 'System info and Power', hasChild: true, test: 'ui/handheld/tizen/platform/tizen_system_info' },
+			{ title: 'NFC demo', hasChild: true, test: 'ui/handheld/tizen/platform/tizen_nfc' },
 			{ title: 'Messaging', hasChild: true, test: 'ui/handheld/tizen/platform/messaging/messaging' },
-			{ title: 'Media content', hasChild: true, test: 'ui/handheld/tizen/platform/tizen_media_content' },
-			{ title: 'Alarm', hasChild: true, test: 'ui/handheld/tizen/platform/tizen_alarm' },
-			{ title: 'Notification', hasChild: true, test: 'ui/handheld/tizen/platform/tizen_notification' },
+			{ title: 'Media content', hasChild: true, test: 'ui/handheld/tizen/platform/media_content' },
+			{ title: 'Alarm', hasChild: true, test: 'ui/handheld/tizen/platform/alarm' },
+			{ title: 'Notification', hasChild: true, test: 'ui/handheld/tizen/platform/notification' },
 			{ title: 'Calendar', hasChild: true, test: 'ui/handheld/tizen/platform/calendar' }
 		],
 		tableview = Ti.UI.createTableView({
 			data: data
 		});
-	
+
 	tableview.addEventListener('click', function(e){
 		if (e.rowData.test) {
 			var ExampleWindow = require(e.rowData.test),
@@ -26,9 +26,9 @@ function tizen(_args) {
 			_args.containingTab.open(win, { animated: true });
 		}
 	});
-	
+
 	self.add(tableview);
-	
+
 	return self;
 };
 
